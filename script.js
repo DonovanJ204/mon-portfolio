e2872877d8682f20104dc15125e911a167d9454e
DonovanJ204 <donovan.jeannot.stcharles@gmail.com>
2026-01-13 08:33:00 +0000

    feat: Ajout de script.js et première interaction DOM

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Script chargé !");
+
+const titre = document.querySelector('#titre-principal');
+
+titre.addEventListener('click', () => {
+  // Ce code ne s'exécute QUE si l'utilisateur clique.
+  titrePrincipal.textContent = "Vous avez cliqué sur le titre !";
+  console.log('Bouton cliqué !');
+});
